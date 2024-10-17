935470e047570095ce5ad6f3f20709febd9634df
Roy <royamit1998@gmail.com>
2024-10-18 02:15:09 +0300

    Added default view

--- components/my-map.tsx
+++ components/my-map.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import {Map, MapProps} from '@vis.gl/react-google-maps';
 
 interface MyMapProps extends MapProps {
-
 }
 
+const initial = {
+    "lat": 32.07656832175795,
+    "lng": 34.783993916688004,
+    "zoom": 14
+}
 export const MyMap: React.FC<MyMapProps> = (props) => {
     return <Map
-            style={{width: '100vw', height: '100vh'}}
-            defaultCenter={{lat: 27.672932021393862, lng: 85.31184012689732}}
-            defaultZoom={3}
-            gestureHandling={'greedy'}
-            disableDefaultUI={true}
-            {...props}
-        />
+        style={{width: '100vw', height: '100vh'}}
+        defaultCenter={{lat: initial.lat, lng: initial.lng}}
+        defaultZoom={initial.zoom}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+        {...props}
+    />
 }
