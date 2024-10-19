5a43ff764ab6de8eeb8ade003280c6060e8e0cbc
roi nir <royvax96@gmail.com>
2024-10-19 20:32:42 +0300

    implemented user authentication features for the app. also added google sign-in feature.

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import GoogleSignInButton from "@/components/ui/google_btn";
 
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
@@ -36,6 +38,7 @@ export default function Login({ searchParams }: { searchParams: Message }) {
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
           Sign in
         </SubmitButton>
+        <GoogleSignInButton />
         <FormMessage message={searchParams} />
       </div>
     </form>
--- components/profile-avatar.tsx
+++ components/profile-avatar.tsx
@@ -7,9 +7,23 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {ThemeSwitcher} from "@/components/theme-switcher";
-import React from "react";
+import React, { use, useState } from "react";
+import { Button } from "./ui/button";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from 'next/navigation'
 
 export function ProfileAvatar() {
+    const router = useRouter()
+    const handleSignOut = async () => {
+        const supabase = createClient()
+        let { error } = await supabase.auth.signOut();
+        if (error){            
+            console.log(error)
+        } else{
+            router.push('/sign-in')
+        }
+      };
+
     return <DropdownMenu>
         <DropdownMenuTrigger>
             <Avatar className="w-12 h-12 lg:w-16 lg:h-16">
@@ -21,6 +35,7 @@ export function ProfileAvatar() {
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuItem>
                 <ThemeSwitcher/>
+                <Button onClick={handleSignOut}>LOGOUT</Button>
             </DropdownMenuItem>
         </DropdownMenuContent>
     </DropdownMenu>;
