b9278da5db0d930c51af7c75c7ff9be632bf3334
Roy <royamit1998@gmail.com>
2024-10-22 02:39:38 +0300

    Added create parking dialog

--- components/profile-avatar.tsx
+++ components/profile-avatar.tsx
@@ -7,51 +7,73 @@ import {
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {ThemeSwitcher} from "@/components/theme-switcher";
 import React from "react";
-import { Button } from "./ui/button";
-import { createClient } from "@/utils/supabase/client";
-import { useRouter } from 'next/navigation'
+import {Button} from "./ui/button";
+import {createClient} from "@/utils/supabase/client";
+import {useRouter} from 'next/navigation'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 export function ProfileAvatar() {
     const router = useRouter()
 
     const handleSignOut = async () => {
         const supabase = createClient()
-        let { error } = await supabase.auth.signOut();
-        if (error){            
+        let {error} = await supabase.auth.signOut();
+        if (error) {
             console.log(error)
-        } else{
+        } else {
             router.push('/sign-in')
         }
-      };
+    };
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Avatar className="w-12 h-12 lg:w-16 lg:h-16">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>YOUR PROFILE NAME</AvatarFallback>
-                </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="flex flex-col">
-                <DropdownMenuItem className="flex justify-center">
-                    <ThemeSwitcher />
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex justify-center">
-                    <Button className="w-full" onClick={() => {/* Add your create parking spot logic here */}}>
-                        Create Parking Spot
-                    </Button>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex justify-center">
-                    <Button className="w-full" onClick={() => {/* Add user info logic here */}}>
-                        User Info
-                    </Button>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex justify-center">
-                    <Button className="w-full" onClick={handleSignOut}>
-                        Logout
-                    </Button>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar className="w-12 h-12 lg:w-16 lg:h-16">
+                        <AvatarImage src="https://github.com/shadcn.png"/>
+                        <AvatarFallback>YOUR PROFILE NAME</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="flex flex-col">
+                    <DropdownMenuItem className="flex justify-center">
+                        <ThemeSwitcher/>
+                    </DropdownMenuItem>
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem className="flex justify-center">
+                            <Button className="w-full">
+                                Create Parking Spot
+                            </Button>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="flex justify-center">
+                        <Button className="w-full" onClick={() => {/* Add user info logic here */
+                        }}>
+                            User Info
+                        </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="flex justify-center">
+                        <Button className="w-full" onClick={handleSignOut}>
+                            Logout
+                        </Button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create a new Parking Spot</DialogTitle>
+                    <DialogDescription>
+                        Add your vehicle details, location, and payment method.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     );
 }
