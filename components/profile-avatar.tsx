d0be3ff7a92e12d49e61d96fc69b18d27b90e845
Roy Amit <101219297+royamit1@users.noreply.github.com>
2024-10-22 02:27:45 +0300

    Profile (#6)
    * Editing the profile dropdown menu - adding create parking spot, user info and logout
    
    * Fixed warnings for unused imports

--- components/profile-avatar.tsx
+++ components/profile-avatar.tsx
@@ -2,18 +2,18 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {ThemeSwitcher} from "@/components/theme-switcher";
-import React, { use, useState } from "react";
+import React from "react";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from 'next/navigation'
 
 export function ProfileAvatar() {
     const router = useRouter()
+
     const handleSignOut = async () => {
         const supabase = createClient()
         let { error } = await supabase.auth.signOut();
@@ -24,19 +24,34 @@ export function ProfileAvatar() {
         }
       };
 
-    return <DropdownMenu>
-        <DropdownMenuTrigger>
-            <Avatar className="w-12 h-12 lg:w-16 lg:h-16">
-                <AvatarImage src="https://github.com/shadcn.png"/>
-                <AvatarFallback>YOUR PROFILE NAME</AvatarFallback>
-            </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuItem>
-                <ThemeSwitcher/>
-                <Button onClick={handleSignOut}>LOGOUT</Button>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>;
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar className="w-12 h-12 lg:w-16 lg:h-16">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>YOUR PROFILE NAME</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="flex flex-col">
+                <DropdownMenuItem className="flex justify-center">
+                    <ThemeSwitcher />
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex justify-center">
+                    <Button className="w-full" onClick={() => {/* Add your create parking spot logic here */}}>
+                        Create Parking Spot
+                    </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex justify-center">
+                    <Button className="w-full" onClick={() => {/* Add user info logic here */}}>
+                        User Info
+                    </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex justify-center">
+                    <Button className="w-full" onClick={handleSignOut}>
+                        Logout
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 }
