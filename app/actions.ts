5399fbe7c36989f30b631456e8099d1ae8d514c4
Roy <royamit1998@gmail.com>
2024-10-22 05:40:42 +0300

    Added drizzle ORM

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,17 @@
 'use server'
 
 import {ParkingFormSchema} from "@/schemas/parking-form-schema";
+import {db} from "@/db";
+import {parkingTable} from "@/db/schema";
 
 export async function createParkingSpot(data: ParkingFormSchema) {
-    console.log(data)
+    // @ts-ignore
+    await db.insert(parkingTable).values({
+        address: "Dan is the king",
+        longitude: 0,
+        latitude: 0,
+        availableFrom: new Date(),
+        availableUntil: new Date(),
+        price: 10,
+    })
 }
--- schemas/parking-form-schema.tsx
+++ schemas/parking-form-schema.tsx
@@ -1,13 +1,13 @@
 import {z} from "zod";
 
 export const parkingFormSchema = z.object({
-    address: z.string().min(1).max(256),
+    address: z.string().min(1).max(255),
     longitude: z.number(),
     latitude: z.number(),
     availableFrom: z.string().datetime(),
     availableUntil: z.string().datetime(),
     price: z.coerce.number().positive(),
-    description: z.string().max(256),
+    description: z.string().max(255),
 }).refine(data => data.availableFrom <= data.availableUntil, {
     message: "Available until must be after available from",
     path: ["availableFrom", "availableUntil"],
