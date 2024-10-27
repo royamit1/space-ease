85a885269f55e09584035f5f957bf8ae7269404b
Roy <royamit1998@gmail.com>
2024-10-27 21:09:32 +0200

    Added the creating parking form into the create-footer

--- components/create-parking-form.tsx
+++ components/create-parking-form.tsx
@@ -6,7 +6,10 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {createParkingSpot} from "@/app/actions";
 import {parkingFormSchema, ParkingFormSchema} from "@/schemas/parking-form-schema";
-import { redirect } from "next/navigation";
+
+interface CreateParkingFormProps {
+    step: number;
+}
 
 export const CreateParkingForm: React.FC = () => {
     const form = useForm<ParkingFormSchema>({
@@ -21,7 +24,7 @@ export const CreateParkingForm: React.FC = () => {
             address: "",
         },
     })
-
+    const [step, setStep] = useState(1);
     const addressInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
@@ -53,112 +56,157 @@ export const CreateParkingForm: React.FC = () => {
         // close
     }
 
-    return <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
-            <FormField
-                control={form.control}
-                name="address"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Address</FormLabel>
-                        <FormControl>
-                            <Input {...field} ref={addressInputRef} placeholder="Search for an address" />
-                        </FormControl>
-                        <FormDescription>
-                            The parking spot closest address
-                        </FormDescription>
-                        <FormMessage/>
-                    </FormItem>
-                )}/>
-            <div className="flex flex-row gap-4">
-                <FormField
-                    control={form.control}
-                    name="longitude"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Longitude</FormLabel>
-                            <FormControl>
-                                <Input {...field} type="number"/>
-                            </FormControl>
-                            <FormDescription>
-                                (will be selected from the map)
-                            </FormDescription>
-                            <FormMessage/>
-                        </FormItem>
+    const nextStep = () => {
+        setStep((prev) => Math.min(prev + 1, 3)); // Assuming 3 steps
+    };
 
-                    )}/>
-                <FormField
-                    control={form.control}
-                    name="latitude"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Latitude</FormLabel>
-                            <FormControl>
-                                <Input {...field} type="number"/>
-                            </FormControl>
-                            <FormDescription>
-                                (will be selected from the map)
-                            </FormDescription>
-                            <FormMessage/>
-                        </FormItem>
+    const prevStep = () => {
+        setStep((prev) => Math.max(prev - 1, 1));
+    };
 
-                    )}/>
-            </div>
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                {step === 1 && (
+                    <>
+                        <FormField
+                            control={form.control}
+                            name="address"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Address</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            ref={addressInputRef}
+                                            placeholder="Search for an address"
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        The parking spot closest address
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex flex-row gap-4">
+                            <FormField
+                                control={form.control}
+                                name="longitude"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Longitude</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} type="number" />
+                                        </FormControl>
+                                        <FormDescription>
+                                            (will be selected from the map)
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="latitude"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Latitude</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} type="number" />
+                                        </FormControl>
+                                        <FormDescription>
+                                            (will be selected from the map)
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </>
+                )}
 
-            <div className="flex flex-row gap-4">
-                <FormField
-                    control={form.control}
-                    name="availableFrom"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Available From</FormLabel>
-                            <FormControl>
-                                {/* @ts-ignore */}
-                                <Input {...field}/>
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}/>
-                <FormField
-                    control={form.control}
-                    name="availableUntil"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Available Until</FormLabel>
-                            <FormControl>
-                                {/* @ts-ignore */}
-                                <Input {...field}/>
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}/>
-            </div>
+                {step === 2 && (
+                    <div className="flex flex-row gap-4">
+                        <FormField
+                            control={form.control}
+                            name="availableFrom"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Available From</FormLabel>
+                                    <FormControl>
+                                        {/* @ts-ignore */}
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="availableUntil"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Available Until</FormLabel>
+                                    <FormControl>
+                                        {/* @ts-ignore */}
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                )}
 
-            <FormField
-                control={form.control}
-                name="price"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Price</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}/>
-            <FormField
-                control={form.control}
-                name="description"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Description</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}/>
-            <Button type="submit" className="w-full">Create Parking Spot</Button>
-        </form>
-    </Form>
+                {step === 3 && (
+                    <>
+                        <FormField
+                            control={form.control}
+                            name="price"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Price</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </>
+                )}
+
+                <div className="flex justify-between">
+                    {step > 1 && (
+                        <Button type="button" onClick={prevStep}>
+                            Previous
+                        </Button>
+                    )}
+                    {step < 3 ? (
+                        <Button type="button" onClick={nextStep}>
+                            Next
+                        </Button>
+                    ) : (
+                        <Button type="submit" className="w-full">
+                            Create Parking Spot
+                        </Button>
+                    )}
+                </div>
+            </form>
+        </Form>
+    );
 }
