5b2d89a54260ef554821851f6ad5666b2e425861
Roy <royamit1998@gmail.com>
2024-11-22 01:42:58 +0200

    Changed parking list to fetch

--- components/parking-list.tsx
+++ components/parking-list.tsx
@@ -1,19 +1,42 @@
 'use client';
 import React from "react";
-import {ParkingSpot} from "@/prisma/generated/client";
 import {ParkingSpotItem} from "@/components/parking-item";
+import {useParkingSpots} from "@/hooks/useParkingSpots";
+import {ParkingSpotFilters} from "@/app/actions";
+import {AlertCircle} from "lucide-react";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 
 interface ParkingListProps {
-    parkingSpots: ParkingSpot[];
+    filters: ParkingSpotFilters
 }
 
-export const ParkingList: React.FC<ParkingListProps> = ({parkingSpots}) => (
-    <ul className="flex-grow flex flex-col w-full p-4 overflow-y-auto space-y-3">
-        {parkingSpots.map((spot) => (
-            <ParkingSpotItem
-                key={spot.id}
-                spot={spot}
-            />
-        ))}
-    </ul>
-);
+export const ParkingList: React.FC<ParkingListProps> = ({ filters }) => {
+    const {data: parkingSpots, error} = useParkingSpots(filters);
+
+    if (error) {
+        return <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4"/>
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+                An error occurred while fetching parking spots. Please try again later.
+            </AlertDescription>
+        </Alert>
+    }
+    if (parkingSpots) {
+        return (
+            <ul className="flex-grow flex flex-col w-full p-4 overflow-y-auto space-y-3">
+                {parkingSpots.map((spot) => (
+                    <ParkingSpotItem
+                        key={spot.id}
+                        spot={spot}
+                    />
+                ))}
+            </ul>
+        )
+    }
+
+    return <div className="flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-600"></div>
+        <span className="ml-2 text-gray-600 font-medium">Loading, please wait...</span>
+    </div>
+};
