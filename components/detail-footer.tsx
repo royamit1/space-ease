9e275350b6907474c80923800b21fc5341ed5721
Roy <royamit1998@gmail.com>
2024-11-27 00:37:40 +0200

    Fixed calculate distance

--- components/detail-footer.tsx
+++ components/detail-footer.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import React from "react"
 import ParkingDetails from "@/components/parking-details"
 import { useQueryClient } from "@tanstack/react-query"
 import { startRenting } from "@/app/actions"
 import { ConfirmationButton } from "@/components/common/confirmation-button"
-import { Location } from "@/utils/types"
 import { useAuthStatus } from "@/hooks/useAuthStatus"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 
@@ -13,33 +12,9 @@ interface DetailFooterProps {
     selectedParkingSpot: number
 }
 
-const LOCATION_STORAGE_KEY = "userLocation"
-
 export const DetailFooter: React.FC<DetailFooterProps> = ({ selectedParkingSpot }) => {
-    const [currentLocation, setCurrentLocation] = useState<Location | null>(null)
     const queryClient = useQueryClient()
     const isLoggedIn = useAuthStatus() // Use custom hook
-
-    useEffect(() => {
-        // Check if location is saved in localStorage
-        const savedLocation = localStorage.getItem(LOCATION_STORAGE_KEY)
-        if (savedLocation) {
-            setCurrentLocation(JSON.parse(savedLocation))
-        } else if (navigator.geolocation) {
-            // Fetch current location if not saved
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords
-                    const location = { latitude, longitude }
-                    setCurrentLocation(location)
-                    localStorage.setItem(LOCATION_STORAGE_KEY, JSON.stringify(location)) // Save for later use
-                },
-                (error) => {
-                    console.error("Error fetching user location:", error.message)
-                },
-            )
-        }
-    }, [])
 
     const handleRent = async () => {
         try {
@@ -53,11 +28,7 @@ export const DetailFooter: React.FC<DetailFooterProps> = ({ selectedParkingSpot 
 
     return (
         <div className="flex flex-col h-full p-4">
-            {currentLocation ? (
-                <ParkingDetails parkingSpotId={selectedParkingSpot} userLocation={currentLocation} />
-            ) : (
-                <div className="text-center text-muted-foreground">Loading location to calculate distance...</div>
-            )}
+            <ParkingDetails parkingSpotId={selectedParkingSpot} />
             <div className="flex-grow" />
             <TooltipProvider>
                 <Tooltip>
