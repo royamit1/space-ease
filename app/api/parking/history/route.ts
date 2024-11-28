936fcd730befc99c907fe67028817b3b4289cbfe
Roy <royamit1998@gmail.com>
2024-11-28 20:22:53 +0200

    Added current user filter for "my rental history"

--- app/api/parking/history/route.ts
+++ app/api/parking/history/route.ts
@@ -1,9 +1,18 @@
 import { NextResponse } from "next/server"
 import db from "@/lib/db"
+import { createClient } from "@/utils/supabase/server"
 
 export async function GET() {
     try {
+        const supabase = createClient()
+
+        // Authenticate the user
+        const { data: user, error } = await supabase.auth.getUser()
+        if (error || !user) {
+            return NextResponse.json({ message: "User not authenticated" }, { status: 401 })
+        }
         const historyParkingSpots = await db.rentalHistory.findMany({
+            where: { userId: user.user.id },
             orderBy: {
                 startDate: "desc",
             },
